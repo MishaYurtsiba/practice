6dc11366e921598a46236c424b0224072107c3e7
Andrashko <yurii.andrashko@uzhnu.edu.ua>
2022-06-17 19:48:22 +0300

    install script

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,10 @@ const express = require("express");
 
 const app = express();
 
-app.get("*", (req, res   )=>{
-    res.send("Hello");
+app.get("*", (req, res) => {
+  res.send("Hello");
 });
 
-app.listen(3000, ()=>{
-    console.log("Server started on http://localhost:3000");
+app.listen(3000, () => {
+  console.log("Server started on http://localhost:3000");
 });
