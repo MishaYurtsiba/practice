a8d26d22c180f67771bdb9df86ba30c474caddff
Andrashko <yurii.andrashko@uzhnu.edu.ua>
2022-06-22 13:33:31 +0300

    update delete

--- backend/src/faq/controller.js
+++ backend/src/faq/controller.js
@@ -4,12 +4,18 @@ const faqModel = require("./model");
 
 const faqController = {
     get: (req, res) => {
-        res.send(faqModel.read());
+        let faqs = faqModel.read();
+        if (req.query) {
+            if (req.query.question) {
+                faqs = faqs.filter(faq => faq.question.includes(req.query.question));
+            }
+        }
+        res.send(faqs);
     },
     getById: (req, res) => {
         const id = parseInt(req.params.id);
         let faq = faqModel.read().find(el => el.id == id);
-        if (faq){
+        if (faq) {
             faq.count++;
             res.send(faq);
         }
@@ -19,6 +25,20 @@ const faqController = {
         let newFaq = req.body;
         let result = faqModel.create(newFaq);
         res.send(result);
+    },
+    put: (req, res) => {
+        const id = parseInt(req.params.id);
+        let modifiedFaq = faqModel.update(req.body, id);
+        if (modifiedFaq)
+            res.send(modifiedFaq);
+        else res.status(404).send("Not found");
+    },
+    delete: (req, res) => {
+        const id = parseInt(req.params.id);
+        const deletedFaq = faqModel.delete(id);
+        if (deletedFaq)
+            res.send(deletedFaq);
+        else res.status(404).send("Not found");
     }
 }
 
--- backend/src/faq/model.js
+++ backend/src/faq/model.js
@@ -1,6 +1,7 @@
 //CRUD - Create Read Update Delete
 
 const faqModel = {
+    keys: ["question", "answer", "createdAt", "count"],
     data: [
         {
             id: 1,
@@ -15,7 +16,7 @@ const faqModel = {
             answer: "124 - системний аналіз",
             cratedAt: new Date(2022, 6, 21),
             count: 3
-        },
+        }
     ],
     count: 2,
 
@@ -28,7 +29,27 @@ const faqModel = {
         faq.cratedAt = new Date(Date.now());
         faq.count = 0;
         this.data.push(faq);
-        return this.data[this.data.length - 1];
+        return faq;
+    },
+    update: function (faq, id) { //faq = { question: "Найкраща спеціальність?",} id = 5
+        let element = this.data.find(f => f.id == id);
+        if (!element)
+            return null;
+        // if (faq.question)
+        //     element.question = faq.question;
+        // element.answer = faq.answer;
+        // ...
+        for (let key of this.keys)
+            if (faq[key])
+                element[key] = faq[key];
+        return element;
+    },
+    delete: function (id) {
+        const index = this.data.findIndex(f => f.id == id);
+        if (index == -1)
+            return null;
+        const deletedElement = this.data.splice(index, 1);
+        return deletedElement;
     }
 }
 
